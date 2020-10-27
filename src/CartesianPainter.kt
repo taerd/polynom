3125e971a68ad8d0aa7e5ce9a449e7423f5c3ccb
Артур Имамутдинов <ffifalafiesta@gmail.com>
2020-10-27 12:03:15 +0300

    fixed cartesian Painter

--- src/CartesianPainter.kt
+++ src/CartesianPainter.kt
@@ -43,15 +43,8 @@ class CartesianPainter(val plane: CartesianScreenPlane) : Painter {
             val differenceX = (plane.xMax - plane.xMin) * 10.toInt()
             val differenceY = (plane.yMax - plane.yMin) * 10.toInt()
 
-            if (differenceX <= 60) coefficientX = 10.0 else
-                if (differenceX <= 230) coefficientX = 2.0 else
-                    if (differenceX <= 510) coefficientX = 1.0 else
-                        if (differenceX <= 1000) coefficientX = 0.2 //else coefficientX = 0.5
-            if (differenceY <= 60) coefficientY = 10.0 else
-                if (differenceY <= 230) coefficientY = 2.0 else
-                    if (differenceY <= 500) coefficientY = 1.0 else
-                        if (differenceY <= 1000) coefficientY = 0.2 //else coefficientY = 0.5
-
+            if (differenceX <= 60) coefficientX = 10.0 else if (differenceX <= 230) coefficientX = 2.0 else if (differenceX <= 510) coefficientX = 1.0 else if (differenceX <= 1000) coefficientX = 0.2 else coefficientX = 0.1
+            if (differenceY <= 60) coefficientY = 10.0 else if (differenceY <= 230) coefficientY = 2.0 else if (differenceY <= 500) coefficientY = 1.0 else if (differenceY <= 1000) coefficientY = 0.2 else coefficientY = 0.1
 
             if (0.0 <= plane.xMin || 0.0 >= plane.xMax) {
                 for (i in (plane.yMin * coefficientY).toInt()..(plane.yMax * coefficientY).toInt()) {
@@ -68,8 +61,6 @@ class CartesianPainter(val plane: CartesianScreenPlane) : Painter {
                 }
             }
 
-
-
             if (0.0 <= plane.yMin || 0.0 >= plane.yMax) {
                 for (i in (plane.xMin * coefficientX).toInt()..(plane.xMax * coefficientX).toInt()) {
                     val gap = if (i % 10 == 0) 4 else if (i % 5 == 0) 2 else 0
@@ -84,13 +75,11 @@ class CartesianPainter(val plane: CartesianScreenPlane) : Painter {
                     g.drawLine(x, y0 - dt - gap, x, y0 + dt + gap)
                 }
             }
-
-
         }
     }
 
 
-    private fun drawLabels(g: Graphics?) {//числа
+    private fun drawLabels(g: Graphics?) {
         if (g != null) {
 
             g.font = Font("Cambria", Font.BOLD, 12)
@@ -107,24 +96,13 @@ class CartesianPainter(val plane: CartesianScreenPlane) : Painter {
             val differenceX = (plane.xMax - plane.xMin) * 100.toInt()
             val differenceY = (plane.yMax - plane.yMin) * 100.toInt()
 
-            if (differenceX <= 600) coefficientX = 10.0 else
-                if (differenceX <= 2300) coefficientX = 2.0 else
-                    if (differenceX <= 5100) coefficientX = 1.0 else
-                        if (differenceX <= 10000) coefficientX = 0.2 // else coefficientX = 0.5
-            if (differenceY <= 600) coefficientY = 10.0 else
-                if (differenceY <= 2300) coefficientY = 2.0 else
-                    if (differenceY <= 5100) coefficientY = 1.0 else
-                        if (differenceY <= 10000) coefficientY = 0.2 // else coefficientY = 0.5
+            if (differenceX <= 600) coefficientX = 10.0 else if (differenceX <= 2300) coefficientX = 2.0 else if (differenceX <= 5100) coefficientX = 1.0 else if (differenceX <= 10000) coefficientX = 0.2 else coefficientX = 0.1
+            if (differenceY <= 600) coefficientY = 10.0 else if (differenceY <= 2300) coefficientY = 2.0 else if (differenceY <= 5100) coefficientY = 1.0 else if (differenceY <= 10000) coefficientY = 0.2 else coefficientY = 0.1
 
             if (0.0 <= plane.yMin || 0.0 >= plane.yMax) {
-                for (i in (plane.xMin * coefficientX).toInt()..(plane.xMax * coefficientX).toInt()) {//горизонтальные числа
-                    //val x = Converter.xCrt2Scr(plane.xMin + (((plane.xMax-plane.xMin)*i)/20),plane)
-                    val x = Converter.xCrt2Scr(i / coefficientX, plane) //место на котором надо рисовать!!!!!!!!!!!
-                    //val x = Converter.xCrt2Scr((i / 10.0), plane)
-                    //g.drawString((((plane.xMin+(plane.xMax-plane.xMin)*i/20)*10).toInt()*0.1).toString(),x,y0)
-                    //val x1=(((plane.xMin+(plane.xMax-plane.xMin)*i/20))*100).toInt()
+                for (i in (plane.xMin * coefficientX).toInt()..(plane.xMax * coefficientX).toInt()) {
+                    val x = Converter.xCrt2Scr(i / coefficientX, plane)
                     var x1 = Converter.xScr2Crt(x, plane)
-                    //x1=Math.round(x1*100000.0)/100000.0
                     //для более точных значений
                     x1 = Math.round(x1 * 10000.0) / 10000.0
                     x1 = Math.round(x1 * 1000.0) / 1000.0
@@ -132,19 +110,15 @@ class CartesianPainter(val plane: CartesianScreenPlane) : Painter {
                     x1 = Math.round(x1 * 10.0) / 10.0
                     //x1=Math.round(x1*1.0)/1.0
                     val p = m.getStringBounds(x1.toString(), g).width.toInt()
+                    val b = m.getStringBounds(x1.toString(),g).height.toInt()
 
-                    g.drawString(x1.toString(), x - p / 2, Converter.yCrt2Scr(plane.yMin, plane) - 6)
-                    g.drawString(x1.toString(), x - p / 2, Converter.yCrt2Scr(plane.yMax, plane) + 15)
+                    g.drawString(x1.toString(), x - p / 2, Converter.yCrt2Scr(plane.yMin, plane) - b/2)//-6
+                    g.drawString(x1.toString(), x - p / 2, Converter.yCrt2Scr(plane.yMax, plane) + b)//15
                 }
             } else {
-                for (i in (plane.xMin * coefficientX).toInt()..(plane.xMax * coefficientX).toInt()) {//горизонтальные числа
-                    //val x = Converter.xCrt2Scr(plane.xMin + (((plane.xMax-plane.xMin)*i)/20),plane)
-                    val x = Converter.xCrt2Scr(i / coefficientX, plane) //место на котором надо рисовать!!!!!!!!!!!
-                    //val x = Converter.xCrt2Scr((i / 10.0), plane)
-                    //g.drawString((((plane.xMin+(plane.xMax-plane.xMin)*i/20)*10).toInt()*0.1).toString(),x,y0)
-                    //val x1=(((plane.xMin+(plane.xMax-plane.xMin)*i/20))*100).toInt()
+                for (i in (plane.xMin * coefficientX).toInt()..(plane.xMax * coefficientX).toInt()) {
+                    val x = Converter.xCrt2Scr(i / coefficientX, plane)
                     var x1 = Converter.xScr2Crt(x, plane)
-                    //x1=Math.round(x1*100000.0)/100000.0
                     //для более точных значений
                     x1 = Math.round(x1 * 10000.0) / 10000.0
                     x1 = Math.round(x1 * 1000.0) / 1000.0
@@ -152,18 +126,14 @@ class CartesianPainter(val plane: CartesianScreenPlane) : Painter {
                     x1 = Math.round(x1 * 10.0) / 10.0
                     //x1=Math.round(x1*1.0)/1.0
                     val p = m.getStringBounds(x1.toString(), g).width.toInt()
-                    g.drawString(x1.toString(), x - p / 2, y0 - 5)
+                    val b = m.getStringBounds(x1.toString(), g).height.toInt()
+                    g.drawString(x1.toString(), x - p / 2, y0 - b/2)
                 }
             }
 
-
-
-
             if (0.0 <= plane.xMin || 0.0 >= plane.xMax) {
-                for (i in (plane.yMin * coefficientY).toInt()..(plane.yMax * coefficientY).toInt()) {//горизонтальные числа
+                for (i in (plane.yMin * coefficientY).toInt()..(plane.yMax * coefficientY).toInt()) {
                     val y = Converter.yCrt2Scr(i / coefficientY, plane)
-                    //val x = Converter.xCrt2Scr((i / 10.0), plane)
-                    //g.drawString((((plane.xMin+(plane.xMax-plane.xMin)*i/20)*10).toInt()*0.1).toString(),x,y0)
                     var y1 = Converter.yScr2Crt(y, plane)
                     y1 = Math.round(y1 * 10000.0) / 10000.0
                     y1 = Math.round(y1 * 1000.0) / 1000.0
@@ -171,25 +141,25 @@ class CartesianPainter(val plane: CartesianScreenPlane) : Painter {
                     y1 = Math.round(y1 * 10.0) / 10.0
                     //y1=Math.round(y1*1.0)/1.0
                     val p = m.getStringBounds(y1.toString(), g).height.toInt()
+                    val b = m.getStringBounds(y1.toString(),g).width.toInt()
                     if (y1 != 0.0) {
-                        g.drawString(y1.toString(), Converter.xCrt2Scr(plane.xMin, plane) + 8, y + p / 2 - 3)
-                        g.drawString(y1.toString(), Converter.xCrt2Scr(plane.xMax, plane) - 25, y + p / 2 - 3)
+                        g.drawString(y1.toString(), Converter.xCrt2Scr(plane.xMin, plane) + b/2, y + p/4  )//8
+                        g.drawString(y1.toString(), Converter.xCrt2Scr(plane.xMax, plane) - (b*1.5).toInt(), y + p/4 )//-25
                     }
                 }
             } else {
-                for (i in (plane.yMin * coefficientY).toInt()..(plane.yMax * coefficientY).toInt()) {//горизонтальные числа
+                for (i in (plane.yMin * coefficientY).toInt()..(plane.yMax * coefficientY).toInt()) {
                     val y = Converter.yCrt2Scr(i / coefficientY, plane)
-                    //val x = Converter.xCrt2Scr((i / 10.0), plane)
-                    //g.drawString((((plane.xMin+(plane.xMax-plane.xMin)*i/20)*10).toInt()*0.1).toString(),x,y0)
                     var y1 = Converter.yScr2Crt(y, plane)
                     y1 = Math.round(y1 * 10000.0) / 10000.0
                     y1 = Math.round(y1 * 1000.0) / 1000.0
                     y1 = Math.round(y1 * 100.0) / 100.0
                     y1 = Math.round(y1 * 10.0) / 10.0
                     val p = m.getStringBounds(y1.toString(), g).height.toInt()
+                    val b = m.getStringBounds(y1.toString(),g).width.toInt()
                     //y1=Math.round(y1*1.0)/1.0
                     if (y1 != 0.0) {
-                        g.drawString(y1.toString(), x0 + 6, y + p / 2)
+                        g.drawString(y1.toString(), x0 + b/2, y + p / 4)//6
                     }
                 }
             }
